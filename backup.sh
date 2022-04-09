66dea241987bba3d0706b95264adc1182e053803
JerryLin1205 <chinesecolin@qq.com>
2022-04-09 11:59:06 +0800

    Test passed for backup.sh

--- backup.sh
+++ backup.sh
@@ -8,12 +8,12 @@
 #       2.3. Sync nextcloud directory with --link-dest and the dir value was get from step 2.2
 
 TODAY=$(date +%Y-%m-%d)
-SOURCEDIR=/home/ubuntu/nas/data
-DESTDIR=/mnt/wdusb/backuptest
-LASTBACKUPDIR=${DESTDIR}/$(ls ${DESTDIR} | tail -n 1)
-TODAYDIR=${DESTDIR}/nexcloud_${TODAY}
+SOURCEDIR=/www/wwwroot/nextcloud/
+DESTDIR=/mnt/wdusb/ncbackup/
+LASTBACKUPDIR=${DESTDIR}$(ls ${DESTDIR} | tail -n 1)
+TODAYDIR=${DESTDIR}nextcloud_${TODAY}
 EXCLUDELIST='/srv/nextcloud/rsync_exclude.list'
-LOGFILE='/home/ubuntu/nas/backup.log'
+LOGFILE='/srv/nextcloud/backup.log'
 
 echo "---------Date:"$TODAY" start---------"  >> $LOGFILE
 echo "LASTBACKUPDIR:"$LASTBACKUPDIR  >> $LOGFILE
@@ -21,11 +21,11 @@ echo "TODAYDIR:"$TODAYDIR  >> $LOGFILE
 
 if [ ! $(ls -A ${DESTDIR} | tail -n -1) ]; then
 # if not exist, sync all
-    echo "--------Backup Mode: All backup--------" >> $LOGFILE
+    echo "--------Backup Mode: All backup" >> $LOGFILE
     sudo rsync -avz --log-file=$LOGFILE --log-file-format='%t %f %b' --exclude-from=$EXCLUDELIST $SOURCEDIR $TODAYDIR
 else
 # if backup aleady exist, sync only for the increasment
-    echo "--------Backup Mode: Delta backup--------" >> $LOGFILE
+    echo "--------Backup Mode: Delta backup" >> $LOGFILE
     sudo rsync -avz --log-file=$LOGFILE --log-file-format='%t %f %b' --exclude-from=$EXCLUDELIST --link-dest=${LASTBACKUPDIR} $SOURCEDIR $TODAYDIR
 fi
 
